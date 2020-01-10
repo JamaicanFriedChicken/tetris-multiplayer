92c1ede6a77024ec02861ff1beacf36d84341386
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2020-01-10 23:31:03 +0800

    initial draft for websocket communication

--- client/connection-manager.js
+++ client/connection-manager.js
@@ -5,5 +5,11 @@ class ConnectionManager {
 
     connect(address) {
         this.conn = new WebSocket(address);
+
+        this.conn.addEventListener('open', () => {
+            console.log('Connection established.');
+
+            this.conn.send('create-session');
+        });
     }
 }
